a1a6b64443d113c87e75309b2484f464a7ef3423
DayDarkTV <dayofdarkness71@gmail.com>
2024-11-20 21:32:10 -0500

    YOU CAN FRICCIN BREAK COMMAND BLOCKS AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    i finally learned how to overwrite a constructor lol pls praise me

---
+++ src/main/java/com/daysportal/amethystsjoke/mixin/BlocksMixin.java
@@ -0,0 +1,29 @@
+package com.daysportal.amethystsjoke.mixin;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.CommandBlock;
+import net.minecraft.block.MapColor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Blocks.class)
+public abstract class BlocksMixin {
+
+    @Redirect(
+            method = "<clinit>",
+            at = @At(
+                    value = "NEW",
+                    target = "(Lnet/minecraft/block/AbstractBlock$Settings;Z)Lnet/minecraft/block/CommandBlock;"
+            )
+    )
+    private static CommandBlock overwriteCommandBlock(AbstractBlock.Settings settings, boolean auto) {
+        return new CommandBlock(
+                FabricBlockSettings.create()
+                        .mapColor(MapColor.BROWN)
+                        .strength(1.0F, 3600000.0F),
+                true);
+    }
+}
